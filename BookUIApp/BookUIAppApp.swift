870626f22b18ee8912e7c1a4b2bf03790cd15608
salmadwv101 <sfalorifi22@twq.idserve.net>
2022-12-12 21:23:52 +0300

    Initial Commit

---
+++ BookUIApp/BookUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookUIAppApp.swift
+//  BookUIApp
+//
+//  Created by salma alorifi on 18/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BookUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
